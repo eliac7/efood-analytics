375f863f53e2532ebcb55ff9f7a68479b237c380
Ilias <elias.thal7@gmail.com>
2021-11-15 18:07:32 +0200

    Heroku temp folder instead of uploads folder

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 
-const uploadPATH = pathOS.normalize(pathOS.join(__dirname, "uploads/"));
+const uploadPATH = pathOS.normalize(pathOS.join(__dirname, "tmp/"));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
